4913b9a47c39e427eddf0f2e8b0f4a36df6b1aa3
bmerrill2 <bradmerrill@eighty1designs.com>
2016-06-06 23:04:57 -0600

    Changed to gh-pages

---
+++ js/itunesService.js
@@ -0,0 +1,36 @@
+var app = angular.module('itunes');
+
+app.service('itunesService', function ($http, $q) {
+  //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
+  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+
+  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
+  //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+  //Note that in the above line, artist is the parameter being passed in. 
+  //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
+
+  this.getArtist = function (artist) {
+    var defer = $q.defer();
+    $http({
+      method: "JSONP",
+      url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+    }).then(function (results) {
+      console.log(results)
+      var organizeData = [];
+      var result = results.data.results;
+      for (var i = 0; i < result.length; i++) {
+        var artistObject = {
+          AlbumArt: result[i].artworkUrl60,
+          Artist: result[i].artistName,
+          Collection: result[i].collectionName,
+          CollectionPrice:'$' + result[i].collectionPrice,
+          Play: result[i].previewUrl,
+          Type: result[i].kind,
+        };
+        organizeData.push(artistObject);
+      };
+      defer.resolve(organizeData);
+    });
+    return defer.promise
+  };
+});
